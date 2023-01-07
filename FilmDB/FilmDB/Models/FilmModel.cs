5b3e8ff2d5b7f460793fbe4dc125667e5bb87fb0
przemyslawprzybysz <przybysz790@gmail.com>
2023-01-07 13:37:25 +0100

    laboratoria nr 3

---
+++ FilmDB/FilmDB/Models/FilmModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Models
+{
+	public class FilmModel
+	{
+		public int Id { get; set; }
+		public int Year { get; set; }
+		public string Name { get; set; }
+	}
+}
