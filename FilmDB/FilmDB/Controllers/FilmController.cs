dac63574a0c3cef5f7e2920457002700e44b8515
przemyslawprzybysz <przybysz790@gmail.com>
2023-01-08 16:13:18 +0100

    Koniec projektu

--- FilmDB/FilmDB/Controllers/FilmController.cs
+++ FilmDB/FilmDB/Controllers/FilmController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmDB.Logic;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,52 @@ namespace FilmDB.Controllers
 {
 	public class FilmController : Controller
 	{
+		FilmManager filmManager = new FilmManager();
 		public IActionResult Index()
 		{
+			var filmList = filmManager.GetFilms();
+			return View(filmList);
+		}
+
+		[HttpGet]
+		public IActionResult Add()
+		{
 			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Add(FilmModel filmModel)
+		{
+			filmManager.AddFilm(filmModel);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult Remove(int id)
+		{
+			var film = filmManager.GetFilm(id);
+			return View(film);
+		}
+
+		[HttpPost]
+		public IActionResult RemoveConfirm(int id)
+		{
+			filmManager.RemoveFilm(id);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			var film = filmManager.GetFilm(id);
+			return View(film);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(FilmModel film)
+		{
+			filmManager.UpdateFilm(film);
+			return RedirectToAction("Index");
 		}
 	}
 }
---
+++ FilmDB/FilmDB/Logic/FilmManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmDB.Models;
+
+namespace FilmDB.Logic
+{
+	public class FilmManager
+	{
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                context.Add(filmModel);
+                context.SaveChanges();
+            }
+                return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            using (var context = new FilmContext())
+            {
+                var filmToDelete = context.Films.SingleOrDefault(x => x.Id == id);
+                context.Films.Remove(filmToDelete);
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            using (var context = new FilmContext())
+            {
+                context.Films.Update(filmModel);
+                context.SaveChanges();
+            }
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            using (var context = new FilmContext())
+            {
+                var filmToChangeTitle = context.Films.Single(x => x.Id == id);
+                if (string.IsNullOrEmpty(newTitle))
+                {
+                    newTitle = "Brak tytułu";
+                }
+                filmToChangeTitle.Name = newTitle;
+                this.UpdateFilm(filmToChangeTitle);
+            }
+            return this;
+        }
+
+        public FilmModel GetFilm(int id)
+        {
+            FilmModel filmById;
+            using (var context = new FilmContext())
+            {
+                filmById = context.Films.Single(x => x.Id == id);
+            }
+            return filmById;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            using (var context = new FilmContext())
+            {
+                var filmList = context.Films.ToList();
+                return filmList;
+            }
+        }
+    }
+}
